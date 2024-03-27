7a360e5232a328bda575ef9cd06d70c4a295d04f
Davidps9 <david.pujol.santander@gmail.com>
2024-03-27 15:42:38 +0100

    Refactor file paths and imports

---
+++ components/src/card/SCard.tsx
@@ -0,0 +1,19 @@
+import { SCardsProps } from '../@types/cardTypes';
+import assignCardStyles from '../utils/card/assignCardStyles';
+import assignFontColor from '../utils/text/assignFontColor';
+
+export default function SCard({ backgroundColor = 'black', limitText = 0, textColor = 'white', borderColor = 'white', rounded = true, hoverable = true, image, text, title, subtitle, size = 'medium' }: SCardsProps) {
+
+    const cardstyle = assignCardStyles({ backgroundColor, borderColor, rounded, hoverable, size, cardStyles: '' });
+    return (
+        <div className={cardstyle + ' text-left'}>
+            {image && <img className=' object-cover h-1/2 w-full' src={image} alt="card image" />}
+            <span className='px-4 py-2 flex flex-col gap-4'>
+                {title && <h2 className={assignFontColor(textColor, '') + ' font-bold'}>{title}</h2>}
+                {subtitle && <h3 className={assignFontColor(textColor, '')}>{subtitle}</h3>}
+                {text && <p className={assignFontColor(textColor, '')}>{limitText > 0 ? text.split('[...]', limitText) : text}</p>}
+            </span>
+
+        </div>
+    )
+}
