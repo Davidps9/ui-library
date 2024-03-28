c67e9fdc69f87becfa6f559a3a7c21162a390fd1
Davidps9 <david.pujol.santander@gmail.com>
2024-03-28 17:16:09 +0100

    Remove unused files and configurations

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,8 +1,4 @@
 import './App.css'
-import SAvatar from './avatar/SAvatar'
-import SSwitch from './switchButton/SSwitch'
-import Sbutton from './button/Sbutton'
-import SCard from './card/SCard'
 
 function App() {
 
