bc9cbfcf609b4c7ca52f99528f5141bd91767869
Davidps9 <david.pujol.santander@gmail.com>
2024-04-03 13:17:57 +0200

    Update package version and library name

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'index.ts'),
-      name: 'simple-ui-library',
+      name: 'simple-material-ui-library',
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
